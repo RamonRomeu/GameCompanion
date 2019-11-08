899bd62e72ff6f93434d1b79c98fcde46d9504b3
Ramon <ramon.romeu@enti.cat>
2019-11-08 11:01:06 +0100

    Added Login

--- GameCompanion/app/src/main/java/com/example/gamecompanion/MainActivity.kt
+++ GameCompanion/app/src/main/java/com/example/gamecompanion/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.gamecompanion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView.setOnNavigationItemSelectedListener{ menuItem ->
+            when(menuItem.itemId){
+                R.id.chat->{
+
+                }
+                R.id.feed->{
+
+                }
+                R.id.profile->{
+                    //Create Fragment
+                    val profileFragment = ProfileFragment()
+                    //Add Fragment to Fragment Container
+                    val fragmentManager = supportFragmentManager
+                    val fragmentTransaction= fragmentManager.beginTransaction()
+                    fragmentTransaction.add(fragmentContainer.id, profileFragment)
+                    fragmentTransaction.commit()
+                }
+                R.id.friends->{
+
+                }
+
+
+            }
+           true
+        }
     }
 }
