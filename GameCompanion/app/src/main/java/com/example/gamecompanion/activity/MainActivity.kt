dddaa5f6b53556a90879a1c4142cd503cd45b277
Ramon <ramon.romeu@enti.cat>
2019-11-22 11:19:18 +0100

    Added ads

--- GameCompanion/app/src/main/java/com/example/gamecompanion/activity/MainActivity.kt
+++ GameCompanion/app/src/main/java/com/example/gamecompanion/activity/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gamecompanion.R
 import com.example.gamecompanion.fragment.ProfileFragment
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import com.google.firebase.analytics.FirebaseAnalytics
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,13 +14,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Load Ad
+        MobileAds.initialize(this){}
+        val adRequest= AdRequest.Builder().build()
+        bannerAdView.loadAd(adRequest)
+
+
+
         bottomNavigationView.setOnNavigationItemSelectedListener{ menuItem ->
+            //Switch menu item id
             when(menuItem.itemId){
                 R.id.chat ->{
 
+                    FirebaseAnalytics.getInstance(this).logEvent("Chat_Tab_Click", null)
                 }
                 R.id.feed ->{
-
+                    FirebaseAnalytics.getInstance(this).logEvent("News_Tab_Click", null)
                 }
                 R.id.profile ->{
                     //Create Fragment
@@ -27,9 +41,11 @@ class MainActivity : AppCompatActivity() {
                     val fragmentTransaction= fragmentManager.beginTransaction()
                     fragmentTransaction.add(fragmentContainer.id, profileFragment)
                     fragmentTransaction.commit()
+
+                    FirebaseAnalytics.getInstance(this).logEvent("Profile_Tab_Click", null)
                 }
                 R.id.friends ->{
-
+                    FirebaseAnalytics.getInstance(this).logEvent("Friends_Tab_Click", null)
                 }
 
 
